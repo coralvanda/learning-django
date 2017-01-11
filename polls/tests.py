2d8026e6f53030570d1b899b6ab70a386cdc1bb3
Eric <coralvanda@gmail.com>
2017-01-11 16:09:34 +0100

    test: Begin adding tests for index view

--- polls/tests.py
+++ polls/tests.py
@@ -2,8 +2,19 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Question
+
+def create_question(question_text, days):
+	"""
+		Creates a question with the given 'question_text' and publshed the
+		given number of 'days' offset to now (negative for questions published)
+		in the past, positive for questions that have yet to be published)
+	"""
+	time = timezone.now() + datetime.timedelta(days=days)
+	return Question.objects.create(question_text=question_text, pub_date=time)
+
 
 class QuestionMethodTests(TestCase):
 
@@ -33,3 +44,15 @@ class QuestionMethodTests(TestCase):
 		time = timezone.now() - datetime.timedelta(hours=1)
 		recent_question = Question(pub_date=time)
 		self.assertIs(recent_question.was_published_recently(), True)
+
+
+class QuestionViewTests(TestCase):
+
+	def test_index_view_with_no_questions(self):
+		"""
+			If no questions exist, an appropriate message should be displayed
+		"""
+		response = self.client.get(reverse('polls:index'))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "No polls are available.")
+		self.assertQuerysetEqual(response.context['latest_question_list'], [])
