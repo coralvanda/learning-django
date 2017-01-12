67b55eb34a7463ced06a1368d23df8ff7c21d42a
Eric <coralvanda@gmail.com>
2017-01-12 11:02:37 +0100

    test: Add new tests for index view

--- polls/tests.py
+++ polls/tests.py
@@ -56,3 +56,47 @@ class QuestionViewTests(TestCase):
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "No polls are available.")
 		self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+	def test_index_view_with_a_past_question(self):
+		"""
+			Questions with a pub_date in the past should be displayed
+			on the index page
+		"""
+		create_question(question_text="Past question.", days=-30)
+		response = self.client.get(reverse('polls:index'))
+		self.assertQuerysetEqual(
+			response.context['latest_question_list'],
+			['<Question: Past question.>'])
+
+	def test_index_view_with_a_future_question(self):
+		"""
+			Questions with a pub_date in the future should not be displayed
+			on the index page
+		"""
+		create_question(question_text="Future question.", days=30)
+		response = self.client.get(reverse('polls:index'))
+		self.assertContains(response, "No polls are available.")
+		self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+	def test_index_view_with_future_question_and_past_question(self):
+		"""
+			Even if both past and future questions exist, only past questions
+			should be displayed
+		"""
+		create_question(question_text="Past question.", days=-30)
+		create_question(question_text="Future question.", days=30)
+		response = self.client.get(reverse('polls:index'))
+		self.assertQuerysetEqual(
+			response.context['latest_question_list'],
+			['<Question: Past question.>'])
+
+	def test_index_view_with_two_past_questions(self):
+		"""
+			The questions index page may display multiple questions
+		"""
+		create_question(question_text="Past question.", days=-30)
+		create_question(question_text="Past question.", days=-5)
+		response = self.client.get(reverse('polls:index'))
+		self.assertQuerysetEqual(
+			response.context['latest_question_list'],
+			['<Question: Past question 2.>', '<Question: Past question 1.>'])
