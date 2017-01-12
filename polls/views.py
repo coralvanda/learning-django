f929692239d90a108a3b119a1cc7732e9e9bbcf9
Eric <coralvanda@gmail.com>
2017-01-12 11:31:23 +0100

    feat: Change results view to hide future-dated questions

--- polls/views.py
+++ polls/views.py
@@ -16,8 +16,10 @@ class IndexView(generic.ListView):
 	context_object_name = 'latest_question_list'
 
 	def get_queryset(self):
-		"""Return the last five published questions (not including those
-		set to be published in the future"""
+		"""
+			Return the last five published questions (not including those
+			set to be published in the future
+		"""
 		return Question.objects.filter(
 			pub_date__lte=timezone.now()
 			).order_by('-pub_date')[:5]
@@ -43,6 +45,12 @@ class ResultsView(generic.DetailView):
 	"""
 	model = Question
 	template_name = 'polls/results.html'
+
+	def get_queryset(self):
+		"""
+			Excludes any questions that aren't published yet
+		"""
+		return Question.objects.filter(pub_date__lte.timezone.now())
 
 
 def vote(request, question_id):
