bef82d7eeca677f5c97f3ba00e67865ea5429eaf
Eric <coralvanda@gmail.com>
2017-01-26 14:28:10 +0100

    fix: Attempt to fix new homepage routing

--- learning_django/urls.py
+++ learning_django/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from learning-django.home import views
 
 urlpatterns = [
-    url(r'^$', 'home.views.index'),
+    url(r'^$', include('home.urls')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
